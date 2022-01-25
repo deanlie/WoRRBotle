5d52c81ce41d3ac4201f0b4406f6ffd12d5b1ab6
Dean Bandes <deanlie@yahoo.com>
2022-01-25 10:30:21 -0500

    Display letters in first guess

--- app.R
+++ app.R
@@ -35,6 +35,15 @@ ui <- fluidPage(
 
 letterTableToDisplay <- function(word1, word2, word3, word4, word5, word6) {
   HTML(paste(tags$h4("Guesses"),
+             tags$table(
+               tags$tr(
+                 tags$td(substr(word1, 1, 1)),
+                 tags$td(substr(word1, 2, 2)),
+                 tags$td(substr(word1, 3, 3)),
+                 tags$td(substr(word1, 4, 4)),
+                 tags$td(substr(word1, 5, 5))
+               )
+             ),
              sep=""))
 }
 
