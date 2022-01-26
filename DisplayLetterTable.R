ce96a5b38ccf28f5ee0a217753977f0ce0710e0b
Dean Bandes <deanlie@yahoo.com>
2022-01-25 20:16:30 -0500

    Updates DisplayLetterTable.R - factors out table row display

--- DisplayLetterTable.R
+++ DisplayLetterTable.R
@@ -1,15 +1,18 @@
 
+tableRowToDisplay <- function(word1) {
+  tags$tr(tags$td(substr(word1, 1, 1), class = "correct"),
+          tags$td(substr(word1, 2, 2)),
+          tags$td(substr(word1, 3, 3)),
+          tags$td(substr(word1, 4, 4)),
+          tags$td(substr(word1, 5, 5))
+  )
+}
+
 letterTableToDisplay <- function(word1, word2, word3, word4, word5, word6) {
   HTML(paste(tags$h4("Guesses"),
              tags$div(
                tags$table(
-                 tags$tr(
-                   tags$td(substr(word1, 1, 1), class = "correct"),
-                   tags$td(substr(word1, 2, 2)),
-                   tags$td(substr(word1, 3, 3)),
-                   tags$td(substr(word1, 4, 4)),
-                   tags$td(substr(word1, 5, 5))
-                 )
+                 tableRowToDisplay(word1),
                )
              ),
              sep=""))
