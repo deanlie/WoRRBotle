86cdb386eb30e6b16bddb8279c0dd64386e29e15
Dean Bandes <deanlie@yahoo.com>
2022-02-05 14:02:35 -0500

    Delete dead code from ButtonForKeyboardLetter

--- R/ButtonForKeyboardLetter.R
+++ R/ButtonForKeyboardLetter.R
@@ -1,15 +1,3 @@
-# buttonForKeyboardLetter <- function(theText, extraClass = NULL) {
-#   if (is.null(extraClass)) {
-#     theClass <- "kbd"
-#   } else {
-#     theClass <- paste("kbd", extraClass)
-#   }
-#   if (is.null(extraClass)) {
-#     tags$td(actionButton(paste0("typed", theText), theText), class="kbd")
-#   } else {
-#     tags$td(actionButton(paste0("typed", theText), theText, class=extraClass), class=theClass)
-#   }
-# }
 
 buttonForKeyboardLetter <- function(theText, extraClass = NULL) {
   if (is.null(extraClass)) {
